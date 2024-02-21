0ae64776e15f01f45a2ed762416fde443458aff9
Ali Polatlı <ali.polatli@outlook.com>
2024-02-21 07:01:40 +0300

    feat: 06_03 date counter developing.

--- course_steps/06_state_events_forms/03_date_counter/src/App.tsx
+++ course_steps/06_state_events_forms/03_date_counter/src/App.tsx
@@ -4,8 +4,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Counter></Counter>
     </div>
   );
+}
+
+
+function Counter(){
+  const [step, setStep] = React.useState<number>(0);
+  const [count, setCount] = React.useState<number>(0);
+
+  return (
+    <React.Fragment>
+      <button onClick={() => setStep((s) => s-1)}>➖</button>
+      <p>Step: {step}</p>
+      <button onClick={() => setStep((s) => s+1)}>➕</button>
+      <br></br>
+      <button onClick={() => setCount((c) =>  c- step)}>➖</button>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount((c) => c + step)}>➕</button>
+      <br></br>
+    </React.Fragment>
+    
+  )
+
+
+
 }
 
 export default App;
